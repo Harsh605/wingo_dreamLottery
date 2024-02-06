e0087e250d5231b2e4de82712745e8d395df5b4b
JAYESH-VIJAY <jayeshvijay649@gmail.com>
2024-02-06 18:01:18 +0530

    code

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,9 +1,13 @@
 const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
-    host: 'localhost',
-    user: 'harsh',
-    password: 'Metablock@123',
+    // host: "185.199.53.72",
+    // user: 'root',
+    // password: 'Metablock@123',
+    // database: 'wingo'
+    host: "localhost",
+    user: 'Jayesh',
+    password: 'Jayesh@123',
     database: 'wingo'
 });
    // "start": "pm2 start src/server.js"
